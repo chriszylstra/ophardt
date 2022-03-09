3ca3852fc1ea96ce8b7d83dbec280fc5079e8200
Chris Zylstra <czylstra02@gmail.com>
2022-03-09 12:20:08 -0500

    wip still

---
+++ Webcam Software/camera_linux.py
@@ -0,0 +1,62 @@
+#LINUX VERSION
+try:
+    import cv2
+except ImportError:
+    print("You need cv2. Install it with 'pip install opencv-python --prefer-binary'")
+    exit()
+    
+import time
+import os
+import pathlib
+from datetime import datetime
+from pathlib import Path
+
+cam = cv2.VideoCapture(0)
+
+now = datetime.now()
+dt = now.strftime("%d_%m_%Y")
+
+delay = input("Enter the delay time (seconds) between photos: ")
+Path("/home/pi/Pictures/images/").mkdir(parents=True, exist_ok = True)
+filepath = "images/" + dt + "/"
+
+
+def clearConsole():
+    command = 'clear'
+    os.system(command)
+    
+clearConsole()
+delay = input("Enter the delay time (seconds) between photos: ")
+clearConsole()
+
+print("----------------------------Webcam Autocapture-----------------------------")
+print("")
+print("Software written by Chris Zylstra. February 2022.")
+print("")
+print("")
+print("WARNING: Leaving this program running may consume large amounts of disk space.")
+print("Images are ~70KB each. (1GB = ~ 14,000 photos)")
+print("")
+print("This program captures and saves the webcam and saves it to a directory.")
+print("Used for Pumpdown testers in creating timelapses.")
+print("")
+print("*****")
+print ("Press CTRL+C in this window to quit.")
+print ("Photos location: /home/pi/Documents/" + filepath)
+print ("Capturing every: " + delay + " seconds.")
+print("*****")
+
+
+
+try:
+    while True:
+        now = datetime.now()
+        dt_string = now.strftime("%H_%M_%S")
+        ret, image = cam.read()
+        cv2.imwrite('/home/pi/Pictures/images/'+ str(dt_string) + '.jpg',image)
+        time.sleep(int(delay))
+except KeyboardInterrupt:   
+    cam.release()
+    cv2.destroyAllWindows()
+except Exception as e:
+    print(e)
